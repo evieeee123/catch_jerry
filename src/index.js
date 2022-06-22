350c3f0214cc03644fa9ce6a83ea505339f53e97
Evie <evie.zengyiying@gmail.com>
2022-06-21 23:09:25 -0700

    add pop out window1

--- src/index.js
+++ src/index.js
@@ -229,3 +229,40 @@ icon.onclick = function (){
 }
 
 
+// pop out window1
+const openWindow1Button = document.querySelectorAll('[data-window1-target]')
+const closeWindow1Button = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openWindow1Button.forEach(button => {
+    button.addEventListener('click', () => {
+        const window1 = document.querySelector(button.dataset.window1Target)
+        openWindow1(window1)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const windows1 = document.querySelectorAll('.window1.active')
+    windows1.forEach(window1 => {
+        closeWindow1(window1)
+    })
+})
+
+closeWindow1Button.forEach(button => {
+    button.addEventListener('click', () => {
+        const window1 = button.closest('.window1')
+        closeWindow1(window1)
+    })
+})
+
+function openWindow1(window1) {
+    if (window1 === null) return
+    window1.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeWindow1(window1) {
+    if (window1 === null) return
+    window1.classList.remove('active')
+    overlay.classList.remove('active')
+}
